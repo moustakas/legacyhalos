634c678a58d035882107c6ed51c166767eae3541
John Moustakas <john.moustakas@gmail.com>
2018-01-05 09:20:34 -0800

    simplify I/O scripts

--- legacyhalos/io.py
+++ legacyhalos/io.py
@@ -12,42 +12,32 @@ import numpy as np
 from glob import glob
 
 def legacyhalos_dir():
-    key = 'DESI_BASIS_TEMPLATES'
     if 'LEGACYHALOS_DIR' not in os.environ:
-        print('Required ${LEGACYHALOS_DIR environment variable not set'.format(key))
+        print('Required ${LEGACYHALOS_DIR environment variable not set.')
         raise EnvironmentError
     return os.path.abspath(os.getenv('LEGACYHALOS_DIR'))
 
-def coadds_dir():
-    return os.path.abspath(os.path.join(legacyhalos_dir(), 'coadds'))
-
-def findfile(filetype):
-    """Return the complete path to a file of interest.
+def read_catalog(extname='LSPHOT', upenn=True, isedfit=False):
+    """Read the various catalogs.
 
     Args:
-        filetype (str): file type, e.g. 'pix' or 'pixsim'
-
-    Returns:
-        str: full file path to output file
+      upenn - Restrict to the UPenn-matched catalogs.
 
     """
+    import fitsio
+    from astropy.table import Table
 
-    topdir = legacyhalos_dir()
-    coaddsdir = coadds_dir()
+    suffix = ''
+    if isedfit:
+        suffix = '-isedfit'
+    elif upenn:
+        suffix = '-upenn'
+    else:
+        print('Unrecognized suffix!')
+        raise ValueError
 
-    # Definition of where files go
-    location = dict(
-        parent = '{topdir:s}/legacyhalos-parent.fits',
-        upennparent = '{topdir:s}/legacyhalos-upenn-parent.fits',
-        redmapperupenn = '{topdir:s}/sandbox/redmapper-upenn.fits',
-    )
+    catfile = os.path.join(lsdir, 'legacyhalos-parent{}.fits'.format(suffix))
+    
+    cat = Table(fitsio.read(catfile, ext=extname))
 
-    # Check that we know about this kind of file.
-    if filetype not in location:
-        raise ValueError('Unknown filetype {}; known types are {}'.format(filetype, list(location.keys())))
-
-    # Get the outfile location and cleanup extraneous // from path.
-    outfile = location[filetype].format(topdir=legacyhalos_dir(), coaddsdir=coadds_dir())
-    outfile = os.path.normpath(outfile)
-
-    return outfile
+    return cat
