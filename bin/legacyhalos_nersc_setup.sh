0d38428c06e20110b3f75355d876814d16927477
John Moustakas <john.moustakas@gmail.com>
2018-01-05 13:40:14 -0800

    update nersc setup

--- bin/legacyhalos_nersc_setup.sh
+++ bin/legacyhalos_nersc_setup.sh
@@ -4,11 +4,22 @@
 # NERSC.
 
 dr=dr5-new
-desiconda_version=20180103-1.2.3-img
+#desiconda_version=20180103-1.2.3-img
+desiconda_version=20170818-1.1.12-img
+
+echo '$desiconda='$desiconda_version
+#module use /global/common/software/desi/$NERSC_HOST/desiconda/$desiconda_version/modulefiles
+module use /global/common/${NERSC_HOST}/contrib/desi/desiconda/$desiconda_version/modulefiles
+module load desiconda
 
 export LEGACY_SURVEY_DIR=/global/cscratch1/sd/desiproc/$dr
 export LEGACYPIPE_DIR=${CSCRATCH}/repos/legacypipe
+export LEGACYHALOS_DIR=${CSCRATCH}/legacyhalos
 export LEGACYHALOS_CODE_DIR=${CSCRATCH}/repos/legacyhalos
+
+echo '$LEGACYHALOS_DIR='$LEGACYHALOS_DIR
+echo '$LEGACYHALOS_CODE_DIR='$LEGACYHALOS_CODE_DIR
+echo '$LEGACY_SURVEY_DIR='$LEGACY_SURVEY_DIR
 
 export PATH=$LEGACYPIPE_DIR/bin:$PATH
 export PATH=$LEGACYHALOS_CODE_DIR/bin:$PATH
@@ -19,10 +30,6 @@ export PYTHONPATH=$LEGACYHALOS_CODE_DIR:$PYTHONPATH
 export OMP_NUM_THREADS=1
 export MPICH_GNI_FORK_MODE=FULLCOPY
 export KMP_AFFINITY=disabled
-
-echo 'Loading software version desiconda/'$desiconda_version
-module use /global/common/software/desi/$NERSC_HOST/desiconda/$desiconda_version/modulefiles
-module load desiconda
 
 if [ "$NERSC_HOST" == "cori" ]; then
   module use $LEGACYPIPE_DIR/bin/modulefiles/cori
