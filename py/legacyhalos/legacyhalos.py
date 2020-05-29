70614be155feeea9d99f17ae5247abcf2abe2daf
John Moustakas <john.moustakas@gmail.com>
2020-05-29 06:31:41 -0400

    lots more cleanup of SGA module

--- py/legacyhalos/legacyhalos.py
+++ py/legacyhalos/legacyhalos.py
@@ -13,6 +13,171 @@ RACOLUMN = 'RA'
 DECCOLUMN = 'DEC'
 DIAMCOLUMN = 'TBD'
 
+def legacyhalos_dir():
+    if 'LEGACYHALOS_DIR' not in os.environ:
+        print('Required ${LEGACYHALOS_DIR environment variable not set.')
+        raise EnvironmentError
+    ldir = os.path.abspath(os.getenv('LEGACYHALOS_DIR'))
+    if not os.path.isdir(ldir):
+        os.makedirs(ldir, exist_ok=True)
+    return ldir
+
+def legacyhalos_data_dir():
+    if 'LEGACYHALOS_DATA_DIR' not in os.environ:
+        print('Required ${LEGACYHALOS_DATA_DIR environment variable not set.')
+        raise EnvironmentError
+    ldir = os.path.abspath(os.getenv('LEGACYHALOS_DATA_DIR'))
+    if not os.path.isdir(ldir):
+        os.makedirs(ldir, exist_ok=True)
+    return ldir
+
+def legacyhalos_html_dir():
+    if 'LEGACYHALOS_HTML_DIR' not in os.environ:
+        print('Required ${LEGACYHALOS_HTML_DIR environment variable not set.')
+        raise EnvironmentError
+    ldir = os.path.abspath(os.getenv('LEGACYHALOS_HTML_DIR'))
+    if not os.path.isdir(ldir):
+        os.makedirs(ldir, exist_ok=True)
+    return ldir
+
+def sample_dir():
+    sdir = os.path.join(legacyhalos_dir(), 'sample')
+    if not os.path.isdir(sdir):
+        os.makedirs(sdir, exist_ok=True)
+    return sdir
+
+def smf_dir(figures=False, data=False):
+    pdir = os.path.join(legacyhalos_dir(), 'science', 'smf')
+    if not os.path.isdir(pdir):
+        os.makedirs(pdir, exist_ok=True)
+    if figures:
+        pdir = os.path.join(pdir, 'figures')
+        if not os.path.isdir(pdir):
+            os.makedirs(pdir, exist_ok=True)
+    if data:
+        pdir = os.path.join(pdir, 'data')
+        if not os.path.isdir(pdir):
+            os.makedirs(pdir, exist_ok=True)
+    return pdir
+
+def profiles_dir(figures=False, data=False):
+    pdir = os.path.join(legacyhalos_dir(), 'science', 'profiles')
+    if not os.path.isdir(pdir):
+        os.makedirs(pdir, exist_ok=True)
+    if figures:
+        pdir = os.path.join(pdir, 'figures')
+        if not os.path.isdir(pdir):
+            os.makedirs(pdir, exist_ok=True)
+    if data:
+        pdir = os.path.join(pdir, 'data')
+        if not os.path.isdir(pdir):
+            os.makedirs(pdir, exist_ok=True)
+    return pdir
+
+def get_integrated_filename():
+    """Return the name of the file containing the integrated photometry."""
+    if hsc:
+        import legacyhalos.hsc
+        integratedfile = os.path.join(legacyhalos.hsc.hsc_dir(), 'integrated-flux.fits')
+    else:
+        integratedfile = os.path.join(profiles_dir(data=True), 'integrated-flux.fits')
+    return integratedfile
+
+def read_integrated_flux(first=None, last=None, integratedfile=None, verbose=False):
+    """Read the output of legacyhalos.integrate.
+    
+    """
+    if integratedfile is None:
+        integratedfile = get_integrated_filename()
+        
+    if not os.path.isfile(integratedfile):
+        print('File {} not found.'.format(integratedfile)) # non-catastrophic error is OK
+        return None
+    
+    if first and last:
+        if first > last:
+            print('Index first cannot be greater than index last, {} > {}'.format(first, last))
+            raise ValueError()
+    ext = 1
+    info = fitsio.FITS(integratedfile)
+    nrows = info[ext].get_nrows()
+
+    if first is None:
+        first = 0
+    if last is None:
+        last = nrows
+        rows = np.arange(first, last)
+    else:
+        if last >= nrows:
+            print('Index last cannot be greater than the number of rows, {} >= {}'.format(last, nrows))
+            raise ValueError()
+        rows = np.arange(first, last + 1)
+    results = Table(info[ext].read(rows=rows, upper=True))
+    
+    if verbose:
+        if len(rows) == 1:
+            print('Read galaxy index {} from {}'.format(first, integratedfile))
+        else:
+            print('Read galaxy indices {} through {} (N={}) from {}'.format(
+                first, last, len(results), integratedfile))
+            
+    return results
+
+def get_galaxy_galaxydir(cat, datadir=None, htmldir=None, html=False,
+                         candidates=False):
+    """Retrieve the galaxy name and the (nested) directory.
+
+    """
+    import astropy
+    import healpy as hp
+    from legacyhalos.misc import radec2pix
+    
+    nside = 8 # keep hard-coded
+    
+    if datadir is None:
+        datadir = legacyhalos_data_dir()
+    if htmldir is None:
+        htmldir = legacyhalos_html_dir()
+
+    def get_healpix_subdir(nside, pixnum, datadir):
+        subdir = os.path.join(str(pixnum // 100), str(pixnum))
+        return os.path.abspath(os.path.join(datadir, str(nside), subdir))
+
+    if type(cat) is astropy.table.row.Row:
+        ngal = 1
+        if candidates:
+            galaxy = ['{:07d}-{:09d}'.format(cat['MEM_MATCH_ID'], cat['ID'])]
+        else:
+            galaxy = ['{:07d}-{:09d}'.format(cat['MEM_MATCH_ID'], cat['ID_CENT'][0])]
+        pixnum = [radec2pix(nside, cat['RA'], cat['DEC'])]
+    else:
+        ngal = len(cat)
+        if candidates:
+            galaxy = np.array( ['{:07d}-{:09d}'.format(mid, cid)
+                                for mid, cid in zip(cat['MEM_MATCH_ID'], cat['ID'])] )
+        else:
+            galaxy = np.array( ['{:07d}-{:09d}'.format(mid, cid)
+                                for mid, cid in zip(cat['MEM_MATCH_ID'], cat['ID_CENT'][:, 0])] )
+
+        pixnum = radec2pix(nside, cat['RA'], cat['DEC']).data
+
+    galaxydir = np.array([os.path.join(get_healpix_subdir(nside, pix, datadir), gal)
+                          for pix, gal in zip(pixnum, galaxy)])
+    if html:
+        htmlgalaxydir = np.array([os.path.join(get_healpix_subdir(nside, pix, htmldir), gal)
+                                  for pix, gal in zip(pixnum, galaxy)])
+
+    if ngal == 1:
+        galaxy = galaxy[0]
+        galaxydir = galaxydir[0]
+        if html:
+            htmlgalaxydir = htmlgalaxydir[0]
+
+    if html:
+        return galaxy, galaxydir, htmlgalaxydir
+    else:
+        return galaxy, galaxydir
+
 def missing_files(sample, filetype='coadds', size=1, htmldir=None,
                   sdss=False, clobber=False):
     """Find missing data of a given filetype."""    
@@ -102,3 +267,316 @@ def mpi_args():
     args = parser.parse_args()
 
     return args
+
+def write_results(lsphot, results=None, sersic_single=None, sersic_double=None,
+                  sersic_exponential=None, sersic_single_nowavepower=None,
+                  sersic_double_nowavepower=None, sersic_exponential_nowavepower=None,
+                  clobber=False, verbose=False):
+    """Write out the output of legacyhalos-results
+
+    """
+    from astropy.io import fits
+    
+    lsdir = legacyhalos_dir()
+    resultsfile = os.path.join(lsdir, 'legacyhalos-results.fits')
+    if not os.path.isfile(resultsfile) or clobber:
+
+        hx = fits.HDUList()
+
+        hdu = fits.table_to_hdu(lsphot)
+        hdu.header['EXTNAME'] = 'LHPHOT'
+        hx.append(hdu)
+
+        for tt, name in zip( (results, sersic_single, sersic_double, sersic_exponential,
+                              sersic_single_nowavepower, sersic_double_nowavepower,
+                              sersic_exponential_nowavepower),
+                              ('results', 'sersic_single', 'sersic_double', 'sersic_exponential',
+                              'sersic_single_nowavepower', 'sersic_double_nowavepower',
+                              'sersic_exponential_nowavepower') ):
+            hdu = fits.table_to_hdu(tt)
+            hdu.header['EXTNAME'] = name.upper()
+            hx.append(hdu)
+
+        if verbose:
+            print('Writing {}'.format(resultsfile))
+        hx.writeto(resultsfile, overwrite=True)
+    else:
+        print('File {} exists.'.format(resultsfile))
+
+def read_results(first=None, last=None, verbose=False, extname='RESULTS', rows=None):
+    """Read the output of io.write_results.
+
+    """
+    lsdir = legacyhalos_dir()
+    resultsfile = os.path.join(lsdir, 'legacyhalos-results.fits')
+
+    if not os.path.isfile(resultsfile):
+        print('File {} not found.'.format(resultsfile))
+        return None
+    else:
+        if rows is not None:
+            results = Table(fitsio.read(resultsfile, ext=extname, rows=rows))
+        else:
+            results = Table(fitsio.read(resultsfile, ext=extname))
+        if verbose:
+            print('Read {} objects from {} [{}]'.format(len(results), resultsfile, extname))
+        return results
+
+def read_jackknife(verbose=False, dr='dr6-dr7'):
+    """Read the jackknife table (written by legacyhalos-sample-selection.ipynb).
+
+    """
+    jackfile = os.path.join(sample_dir(), 'legacyhalos-jackknife-{}.fits'.format(dr))
+
+    if not os.path.isfile(jackfile):
+        print('File {} not found.'.format(jackfile))
+        return None, None
+
+    jack, hdr = fitsio.read(jackfile, extname='JACKKNIFE', header=True)
+    nside = hdr['NSIDE']
+    
+    if verbose:
+        print('Read {} rows from {}'.format(len(jack), jackfile))
+    return Table(jack), nside
+
+def read_sample(first=None, last=None, dr='dr6-dr7', sfhgrid=1,
+                isedfit_lsphot=False, isedfit_sdssphot=False,
+                isedfit_lhphot=False, candidates=False,
+                kcorr=False, verbose=False):
+    """Read the sample.
+
+    """
+    if candidates:
+        prefix = 'candidate-centrals'
+    else:
+        prefix = 'centrals'
+
+    if isedfit_lsphot:
+        samplefile = os.path.join(sample_dir(), '{}-sfhgrid{:02d}-lsphot-{}.fits'.format(prefix, sfhgrid, dr))
+    elif isedfit_sdssphot:
+        samplefile = os.path.join(sample_dir(), '{}-sfhgrid{:02d}-sdssphot-dr14.fits'.format(prefix, sfhgrid))
+    elif isedfit_lhphot:
+        samplefile = os.path.join(sample_dir(), '{}-sfhgrid{:02d}-lhphot.fits'.format(prefix, sfhgrid))
+    else:
+        samplefile = os.path.join(sample_dir(), 'legacyhalos-{}-{}.fits'.format(prefix, dr))
+        
+    if not os.path.isfile(samplefile):
+        print('File {} not found.'.format(samplefile))
+        return None
+
+    if first and last:
+        if first > last:
+            print('Index first cannot be greater than index last, {} > {}'.format(first, last))
+            raise ValueError()
+
+    if kcorr:
+        ext = 2
+    else:
+        ext = 1
+
+    info = fitsio.FITS(samplefile)
+    nrows = info[ext].get_nrows()
+
+    if first is None:
+        first = 0
+    if last is None:
+        last = nrows
+        rows = np.arange(first, last)
+    else:
+        if last >= nrows:
+            print('Index last cannot be greater than the number of rows, {} >= {}'.format(last, nrows))
+            raise ValueError()
+        rows = np.arange(first, last + 1)
+    
+    sample = Table(info[ext].read(rows=rows))
+    if verbose:
+        if len(rows) == 1:
+            print('Read galaxy index {} from {}'.format(first, samplefile))
+        else:
+            print('Read galaxy indices {} through {} (N={}) from {}'.format(
+                first, last, len(sample), samplefile))
+            
+    return sample
+
+def _read_paper_sample(paper='profiles', first=None, last=None, dr='dr8',
+                       sfhgrid=1, isedfit_lsphot=False, isedfit_sdssphot=False,
+                       isedfit_lhphot=False, candidates=False, kcorr=False,
+                       verbose=False):
+    """Wrapper to read a sample for a given paper.
+
+    """
+    if paper == 'profiles':
+        paperdir = profiles_dir(data=True)
+    elif paper == 'smf':
+        paperdir = smf_dir(data=True)
+    else:
+        print('Unrecognized paper {}!'.format(paper))
+        raise ValueError()
+        
+    if candidates:
+        prefix = 'candidate-centrals'
+    else:
+        prefix = 'centrals'
+
+    if isedfit_lsphot:
+        samplefile = os.path.join(paperdir, '{}-{}-sfhgrid{:02d}-lsphot-{}.fits'.format(paper, prefix, sfhgrid, dr))
+    elif isedfit_sdssphot:
+        samplefile = os.path.join(paperdir, '{}-{}-sfhgrid{:02d}-sdssphot-dr14.fits'.format(paper, prefix, sfhgrid))
+    elif isedfit_lhphot:
+        samplefile = os.path.join(paperdir, '{}-{}-sfhgrid{:02d}-lhphot.fits'.format(paper, prefix, sfhgrid))
+    else:
+        samplefile = os.path.join(paperdir, 'sample-{}-{}-{}.fits'.format(paper, prefix, dr))
+        
+    if not os.path.isfile(samplefile):
+        print('File {} not found.'.format(samplefile))
+        return None
+
+    if first and last:
+        if first > last:
+            print('Index first cannot be greater than index last, {} > {}'.format(first, last))
+            raise ValueError()
+
+    if kcorr:
+        ext = 2
+    else:
+        ext = 1
+
+    info = fitsio.FITS(samplefile)
+    nrows = info[ext].get_nrows()
+
+    if first is None:
+        first = 0
+    if last is None:
+        last = nrows
+        rows = np.arange(first, last)
+    else:
+        if last >= nrows:
+            print('Index last cannot be greater than the number of rows, {} >= {}'.format(last, nrows))
+            raise ValueError()
+        rows = np.arange(first, last + 1)
+
+    sample = Table(info[ext].read(rows=rows))
+    if verbose:
+        if len(rows) == 1:
+            print('Read galaxy index {} from {}'.format(first, samplefile))
+        else:
+            print('Read galaxy indices {} through {} (N={}) from {}'.format(
+                first, last, len(sample), samplefile))
+
+    print('Temporary hack to use SDSS coordinates!')
+    from astropy.table import Column
+    sample.add_column(Column(name='RA', data=sample['RA_SDSS']), index=0)
+    sample.add_column(Column(name='DEC', data=sample['DEC_SDSS']), index=1)
+    return sample
+
+def read_smf_sample(first=None, last=None, dr='dr8', sfhgrid=1, isedfit_lsphot=False,
+                    isedfit_sdssphot=False, isedfit_lhphot=False, candidates=False,
+                    kcorr=False, verbose=False):
+    """Read the SMF paper sample.
+
+    """
+    sample = _read_paper_sample(paper='smf', first=first, last=last, dr=dr,
+                                sfhgrid=1, isedfit_lsphot=isedfit_lsphot,
+                                isedfit_sdssphot=isedfit_sdssphot,
+                                isedfit_lhphot=isedfit_lhphot, kcorr=kcorr,
+                                candidates=candidates, verbose=verbose)
+    return sample
+    
+def read_profiles_sample(first=None, last=None, dr='dr8', sfhgrid=1, isedfit_lsphot=False,
+                         isedfit_sdssphot=False, isedfit_lhphot=False, candidates=False,
+                         kcorr=False, verbose=False):
+    """Read the profiles paper sample.
+
+    """
+    sample = _read_paper_sample(paper='profiles', first=first, last=last, dr=dr,
+                                sfhgrid=1, isedfit_lsphot=isedfit_lsphot,
+                                isedfit_sdssphot=isedfit_sdssphot,
+                                isedfit_lhphot=isedfit_lhphot, kcorr=kcorr,
+                                candidates=candidates, verbose=verbose)
+    return sample
+
+def read_redmapper(rmversion='v6.3.1', sdssdr='dr14', index=None, satellites=False,
+                   get_ngal=False):
+    """Read the parent redMaPPer cluster catalog and updated photometry.
+    
+    """
+    from astropy.table import hstack
+    
+    if satellites:
+        suffix1, suffix2 = '_members', '-members'
+    else:
+        suffix1, suffix2 = '', '-centrals'
+    rmfile = os.path.join( os.getenv('REDMAPPER_DIR'), rmversion, 
+                          'dr8_run_redmapper_{}_lgt5_catalog{}.fit'.format(rmversion, suffix1) )
+    rmphotfile = os.path.join( os.getenv('REDMAPPER_DIR'), rmversion, 
+                          'redmapper-{}-lgt5{}-sdssWISEphot-{}.fits'.format(rmversion, suffix2, sdssdr) )
+
+    if get_ngal:
+        ngal = fitsio.FITS(rmfile)[1].get_nrows()
+        return ngal
+    
+    rm = Table(fitsio.read(rmfile, ext=1, upper=True, rows=index))
+    rmphot = Table(fitsio.read(rmphotfile, ext=1, upper=True, rows=index))
+
+    print('Read {} galaxies from {}'.format(len(rm), rmfile))
+    print('Read {} galaxies from {}'.format(len(rmphot), rmphotfile))
+    
+    rm.rename_column('RA', 'RA_REDMAPPER')
+    rm.rename_column('DEC', 'DEC_REDMAPPER')
+    rmphot.rename_column('RA', 'RA_SDSS')
+    rmphot.rename_column('DEC', 'DEC_SDSS')
+    rmphot.rename_column('OBJID', 'SDSS_OBJID')
+
+    assert(np.sum(rmphot['MEM_MATCH_ID'] - rm['MEM_MATCH_ID']) == 0)
+    if satellites:
+        assert(np.sum(rmphot['ID'] - rm['ID']) == 0)
+        rm.remove_columns( ('ID', 'MEM_MATCH_ID') )
+    else:
+        rm.remove_column('MEM_MATCH_ID')
+    rmout = hstack( (rmphot, rm) )
+    del rmphot, rm
+
+    # Add a central_id column
+    #rmout.rename_column('MEM_MATCH_ID', 'CENTRAL_ID')
+    #cid = ['{:07d}'.format(cid) for cid in rmout['MEM_MATCH_ID']]
+    #rmout.add_column(Column(name='CENTRAL_ID', data=cid, dtype='U7'), index=0)
+    
+    return rmout
+
+def literature(kravtsov=True, gonzalez=False):
+    """Assemble some data from the literature here.
+
+    """
+    from colossus.halo import mass_defs
+
+    if kravtsov:
+        krav = dict()
+        krav['m500c'] = np.log10(np.array([15.6,10.3,7,5.34,2.35,1.86,1.34,0.46,0.47])*1e14)
+        krav['mbcg'] = np.array([3.12,4.14,3.06,1.47,0.79,1.26,1.09,0.91,1.38])*1e12
+        krav['mbcg'] = krav['mbcg']*0.7**2 # ????
+        krav['mbcg_err'] = np.array([0.36,0.3,0.3,0.13,0.05,0.11,0.06,0.05,0.14])*1e12
+        krav['mbcg_err'] = krav['mbcg_err'] / krav['mbcg'] / np.log(10)
+        krav['mbcg'] = np.log10(krav['mbcg'])
+
+        M200c, _, _ = mass_defs.changeMassDefinition(10**krav['m500c'], 3.5, 0.0, '500c', '200c')
+        krav['m200c'] = np.log10(M200c)
+
+        return krav
+
+    if gonzalez:
+        gonz = dict()
+        gonz['mbcg'] = np.array([0.84,0.87,0.33,0.57,0.85,0.60,0.86,0.93,0.71,0.81,0.70,0.57])*1e12*2.65
+        gonz['mbcg'] = gonz['mbcg']*0.7**2 # ????
+        gonz['mbcg_err'] = np.array([0.03,0.09,0.01,0.01,0.14,0.03,0.03,0.05,0.07,0.12,0.02,0.01])*1e12*2.65
+        gonz['m500c'] = np.array([2.26,5.15,0.95,3.46,3.59,0.99,0.95,3.23,2.26,2.41,2.37,1.45])*1e14
+        gonz['m500c_err'] = np.array([0.19,0.42,0.1,0.32,0.28,0.11,0.1,0.19,0.23,0.18,0.24,0.21])*1e14
+        gonz['mbcg_err'] = gonz['mbcg_err'] / gonz['mbcg'] / np.log(10)
+
+        M200c, _, _ = mass_defs.changeMassDefinition(gonz['m500c'], 3.5, 0.0, '500c', '200c')
+        
+        gonz['m200c'] = np.log10(M200c)
+        gonz['m500c'] = np.log10(gonz['m500c'])
+        gonz['mbcg'] = np.log10(gonz['mbcg'])
+
+        return gonz
+
