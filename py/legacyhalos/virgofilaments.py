7ae9940c42426e60489a1e41b55bf3a4724b8a5d
John Moustakas <john.moustakas@gmail.com>
2022-10-17 08:26:28 -0700

    change delta-log sma to 4 for virgofilaments

--- py/legacyhalos/virgofilaments.py
+++ py/legacyhalos/virgofilaments.py
@@ -909,7 +909,7 @@ def call_ellipse(onegal, galaxy, galaxydir, pixscale=0.262, nproc=1,
                                           sky_tests=sky_tests, verbose=verbose)
 
     maxsma = None
-    delta_logsma = 8 # 4 # 10
+    delta_logsma = 4 # 10
 
     #igal = 0
     #maxis = data['mge'][igal]['majoraxis'] # [pixels]
