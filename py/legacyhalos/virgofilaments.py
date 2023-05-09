b71b959d72aabf71ab07ff64dae7d75d372d0386
John Moustakas <john.moustakas@gmail.com>
2023-05-09 03:28:29 -0700

    final virgofilaments sample cleanup

--- py/legacyhalos/virgofilaments.py
+++ py/legacyhalos/virgofilaments.py
@@ -971,7 +971,7 @@ def call_ellipse(onegal, galaxy, galaxydir, pixscale=0.262, nproc=1,
                                           sky_tests=sky_tests, verbose=verbose)
 
     maxsma = None
-    delta_logsma = 4 # 10
+    delta_logsma = 4 # 10 # 20
 
     #igal = 0
     #maxis = data['mge'][igal]['majoraxis'] # [pixels]
