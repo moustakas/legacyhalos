30a729591a0622ff570e64b91fa50ec0e5ac8da1
Zhou <zchusre@gmail.com>
2024-07-03 19:48:45 -0700

    update setup

--- setup.py
+++ setup.py
@@ -9,11 +9,24 @@
 
 import os, glob
 from setuptools import setup, find_packages
+import subprocess
+
+# def _get_version():
+#     import subprocess
+#     version = subprocess.check_output('git describe', shell=True)
+#     version = version.decode('utf-8').replace('\n', '')
+#     return version
 
 def _get_version():
-    import subprocess
-    version = subprocess.check_output('git describe', shell=True)
-    version = version.decode('utf-8').replace('\n', '')
+    try:
+        version = subprocess.check_output(['git', 'describe', '--tags'], stderr=subprocess.STDOUT).strip()
+        version = version.decode('utf-8')
+        # Convert version to PEP 440 compliant
+        if '-' in version:
+            parts = version.split('-')
+            version = "{}.dev{}".format(parts[0], parts[1])
+    except subprocess.CalledProcessError:
+        version = "0.0.1"  # Default version if git describe fails
     return version
 
 version = _get_version()
@@ -40,8 +53,12 @@ setup_kwargs=dict(
 setup_kwargs['packages'] = find_packages('py')
 setup_kwargs['package_dir'] = {'':'py'}
 
+def get_scripts(directory):
+    return [script for script in glob.glob(os.path.join(directory, '*')) if os.path.isfile(script)]
+
+
 #- Treat everything in bin/ as a script to be installed
-setup_kwargs['scripts'] = glob.glob(os.path.join('bin', '*'))
+setup_kwargs['scripts'] = get_scripts('bin')
 
 #- Data to include
 # setup_kwargs['package_data'] = {
